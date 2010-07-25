d45e9fec117b1df51b970c07bb75fcb9ea2dce54
Jishnu <jishnu7@gmail.com>
2010-07-25 10:51:11 +0530

    Added README File

--- library.py
+++ library.py
@@ -1,3 +1,20 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 
 bl_addon_info = {
     'name': 'Add Mesh: Integrated Library',
@@ -8,6 +25,21 @@ bl_addon_info = {
     'description': 'Integrated Library',
     'category': 'Add Mesh'}
 
+__bpydoc__ = """
+Integrated Library
+
+This add-on is mainly aimed at saving stages of mesh creation and also saving 
+some pre defined objects.
+
+Usage:
+
+This functionality can be accessed via the "Tool Shelf" in 3D View ([T] key).
+
+Version history:
+
+v0.1 - Initial revision. Seems to work fine for most purposes.
+
+""""
 import bpy
 from bpy.props import *
 
